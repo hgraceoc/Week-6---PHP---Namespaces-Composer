f5cc14f903b6766f22e7759adc01450210bbd38a
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-07 10:31:17 +0100

    hello.php

---
+++ app/hello.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+class Hello 
+{
+
+    public function hello(string $name)
+    {
+        return "Hello, " . $name;
+    }
+}
--- index.php
+++ index.php
@@ -4,3 +4,13 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+
+
+echo "\nQuestion 1:\n";
+
+use App\Hello;
+
+$sayHi = new Hello();
+
+dump($sayHi->hello("Orb")); // "Hello Orb"
+dump($sayHi->hello("Horse")); // "Hello Horse"
